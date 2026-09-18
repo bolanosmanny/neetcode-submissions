c265f2af6646f6f78c83b1bc5ae8a9d2359cf506
Emanuel Mora <180501265+bolanosmanny@users.noreply.github.com>
2026-09-17 23:31:52 -0400

    Update: products-of-array-discluding-self - submission-4

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -1,20 +1,18 @@
 class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
 
-    def encode(self, strs: List[str]) -> str:
-        res = ""
-        for s in strs:
-            res += str(len(s)) + "#" + s
-        return res
+        prefix = 1 
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
 
-    def decode(self, s: str) -> List[str]:
-        res, i = [], 0
-
-        while i < len(s):
-            j = i
-            while(s[j] != "#"):
-                j +=1
-            length = int(s[i:j])
-            res.append(s[j + 1: j + 1 + length])
-            i = j + 1 + length
+        postfix = 1 
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
             
+
+
+        
